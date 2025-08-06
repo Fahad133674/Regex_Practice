49c1a0854b49a71c201080327a38246b6572bb12
Fahad133674 <mdfahad133674@gmail.com>
2025-08-06 13:39:41 +0600

    this is first program

--- 1basic_reg.py
+++ 1basic_reg.py
@@ -1,3 +1,4 @@
+#This is very basic.
 import re
 s = "Afganistan, America,Bangladesh,Canada,Denmark,England,Greenland,Iceland,Netherlands, New Zealand,Sweden,Switzerland"
 land = re.findall("(\w+lands*)",s) #check that word ends with lands or land
